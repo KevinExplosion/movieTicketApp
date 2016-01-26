4bd17db5c21415db55205579f5363201a397f5de
Kevin Mattison and Tim Durward <timothy.a.durward@gmail.com>
2016-01-26 08:55:57 -0800

    add second spec - passed second spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,16 @@
 
-// Example Spec below:
-
-// describe('functionname', function(){
-//   it('first spec here...', function(){
-//     expect(functionname(parameter)).to.equal('placeholder');
-//   });
-// });
-
-
 describe("MovieTicket", function(){
   it('will pass true if newObject if all parameters are met', function(){
-    var newMovieTicket = new MovieTicket("Batman", '12:30', 16, 12.50);
+    var newMovieTicket = new MovieTicket("Batman", '12:30', 16);
     expect(newMovieTicket.name).to.equal("Batman");
     expect(newMovieTicket.time).to.equal("12:30");
     expect(newMovieTicket.age).to.equal(16);
-    expect(newMovieTicket.cost).to.equal(12.50);
+  });
+});
+
+describe("Ticket", function() {
+  it('will return a movie ticket cost of $7 for customers age 55 or older', function() {
+var newMovieTicket = new MovieTicket("Batman", '12:30', 60);
+    expect(newMovieTicket.ticket()).to.equal("$7");
   });
 });
