0f4ca2fcd4302b6224315c4520ddf80d877bbccb
Kevin Mattison and Tim Durward <mattisonii@yahoo.com>
2016-01-26 08:34:37 -0800

    add initials files/setup first spec

---
+++ spec/specs.js
@@ -0,0 +1,19 @@
+
+// Example Spec below:
+
+// describe('functionname', function(){
+//   it('first spec here...', function(){
+//     expect(functionname(parameter)).to.equal('placeholder');
+//   });
+// });
+
+
+describe("MovieTicket", function(){
+  it('will pass true if newObject if all parameters are met', function(
+    var newMovieTicket = new MovieTicket("Batman", '12:30', 16, 12.50);
+    expect(newMovieTicket.name).to.equal("Batman");
+    expect(newMovieTicket.time).to.equal("12:30");
+    expect(newMovieTicket.age).to.equal(16);
+    expect(newMovieTicket.cost).to.equal(12.50);
+  ));
+});
