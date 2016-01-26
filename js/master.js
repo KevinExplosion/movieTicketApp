d14d8ee9792524a8d4a6454dd616204cbaa0485e
Kevin Mattison and Tim Durward <mattisonii@yahoo.com>
2016-01-26 09:03:09 -0800

    Add index.html content.

--- js/master.js
+++ js/master.js
@@ -1,1 +1,17 @@
+//Business Logic; BDD
 
+function MovieTicket(name, time, age){
+  this.name = name;
+  this.time = time;
+  this.age = age;
+};
+
+MovieTicket.prototype.ticket = function(){
+  if (this.age >= 55){
+    return "$7";
+  } else {
+    return "$10";
+  }
+};
+
+//User input Logic
