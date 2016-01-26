811c419cd49b2142daf0fc23c614486a84bd19e9
Kevin Mattison and Tim Durward <timothy.a.durward@gmail.com>
2016-01-26 09:45:50 -0800

    add user input and fixed specs

--- js/master.js
+++ js/master.js
@@ -1,7 +1,8 @@
 //Business Logic; BDD
 
-function MovieTicket(name, time, age){
+function MovieTicket(name, date, time, age){
   this.name = name;
+  this.date = date;
   this.time = time;
   this.age = age;
 };
@@ -15,3 +16,21 @@ MovieTicket.prototype.ticket = function(){
 };
 
 //User input Logic
+
+$(document).ready(function(){
+  $("#buttonSubmit").click(function(event){
+      event.preventDefault();
+
+      //Global Vars;
+      var movie_name = $("#movieName").val();
+      var movie_date = $("#movieDate").val();
+      var movie_time = $("#movieTime").val();
+      var movie_age = parseInt($("#movieAge").val());
+
+      var movie_ticket = new MovieTicket(movie_name, movie_date, movie_time, movie_age);
+
+      $("#movieTickets").append("<p>You are going to see '" + movie_ticket.name + "' on " + movie_ticket.date
+      + " at " + movie_ticket.time + " o'clock. Your ticket holder's age is " + movie_ticket.age + " years old.</p>");
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,7 @@
 
 describe("MovieTicket", function(){
   it('will pass true if newObject if all parameters are met', function(){
-    var newMovieTicket = new MovieTicket("Batman", '12:30', 16);
+    var newMovieTicket = new MovieTicket("Batman","Sept 12th", '12:30', 16);
     expect(newMovieTicket.name).to.equal("Batman");
     expect(newMovieTicket.time).to.equal("12:30");
     expect(newMovieTicket.age).to.equal(16);
@@ -10,7 +10,7 @@ describe("MovieTicket", function(){
 
 describe("Ticket", function() {
   it('will return a movie ticket cost of $7 for customers age 55 or older', function() {
-    var newMovieTicket = new MovieTicket("Batman", '12:30', 60);
+    var newMovieTicket = new MovieTicket("Batman", "Sept 12", '12:30', 60);
     expect(newMovieTicket.ticket()).to.equal("$7");
   });
 });
